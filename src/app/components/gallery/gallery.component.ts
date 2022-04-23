77824b6d9e3dae87500113eb81e636b8cecb049d
Interparvus <michal.holan.on@gmail.com>
2022-04-23 23:46:52 +0200

    parallax update

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,20 +9,49 @@ import { images_data } from 'src/database';
 export class GalleryComponent implements OnInit {
 
   images_data = images_data
-  parallax_types = ['parallaxSlow','parallaxMedium','parallaxHigh']
+  parallax_types = ['parallaxSlow', 'parallaxMedium', 'parallaxHigh']
 
   constructor() { }
 
   ngOnInit(): void {
-    const container = document.getElementById('container');
-    console.log(container);
-    
-    for (let i = 0; i < container!.children.length; i++) {
-      console.log(container!.children[i]);
-  }
+    var card_heights : number[] = []
+
+    window.onload = function () {
+      let newHeight = 30
+      let i = 0
+      document.querySelectorAll(".imageCard").forEach(element => {
+        
+        window.addEventListener('scroll',function(){
+          var value = window.scrollY;
+          
+          if (element.classList.contains('parallaxHigh')){
+            let movingSpeed = value*0.2
+            element.setAttribute('style',`position:relative; top: ${movingSpeed}px`)
+          }
+          else if (element.classList.contains('parallaxMedium')){
+            let movingSpeed = value*0.1
+            element.setAttribute('style',`position:relative; transform: translate3d(0,${movingSpeed}px,0)`)
+          }
+          else if (element.classList.contains('parallaxSlow')){
+            let movingSpeed = value*0.05
+            element.setAttribute('style',`position:relative; transform: translate3d(0,${movingSpeed}px,0)`)
+          }
+        })
+        let newHorizontalChange = Math.floor(Math.random() * 7)+4; 
+        if (i%2 == 0){
+          newHorizontalChange = 0-newHorizontalChange
+        }    
+        newHeight+=Math.floor(Math.random() * 30);
+        element.setAttribute('style', `position:relative; top:${newHeight}px; right:${newHorizontalChange}vw`)
+        card_heights.push(element.clientHeight)
+        i++
+      });
+    }
 
   }
-    parallaxTypeGenerator(){
-      return this.parallax_types[Math.floor(Math.random()*3)]
-    }
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+
 }
