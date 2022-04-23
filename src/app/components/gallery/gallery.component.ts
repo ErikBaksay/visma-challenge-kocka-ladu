1e99ad1ce3eba1c42a7161e1814c11901d3e2866
Interparvus <michal.holan.on@gmail.com>
2022-04-24 01:16:24 +0200

    parallax fix

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -19,24 +19,9 @@ export class GalleryComponent implements OnInit {
     window.onload = function () {
       let newHeight = 30
       let i = 0
-      document.querySelectorAll(".imageCard").forEach(element => {
+      document.querySelectorAll<HTMLElement>(".imageCard").forEach(element => {
         
-        window.addEventListener('scroll',function(){
-          var value = window.scrollY;
-          
-          if (element.classList.contains('parallaxHigh')){
-            let movingSpeed = value*0.2
-            element.setAttribute('style',`position:relative; top: ${movingSpeed}px`)
-          }
-          else if (element.classList.contains('parallaxMedium')){
-            let movingSpeed = value*0.1
-            element.setAttribute('style',`position:relative; transform: translate3d(0,${movingSpeed}px,0)`)
-          }
-          else if (element.classList.contains('parallaxSlow')){
-            let movingSpeed = value*0.05
-            element.setAttribute('style',`position:relative; transform: translate3d(0,${movingSpeed}px,0)`)
-          }
-        })
+
         let newHorizontalChange = Math.floor(Math.random() * 7)+4; 
         if (i%2 == 0){
           newHorizontalChange = 0-newHorizontalChange
@@ -44,6 +29,24 @@ export class GalleryComponent implements OnInit {
         newHeight+=Math.floor(Math.random() * 30);
         element.setAttribute('style', `position:relative; top:${newHeight}px; right:${newHorizontalChange}vw`)
         card_heights.push(element.clientHeight)
+
+        window.addEventListener('scroll',function(){
+          var value = window.scrollY;
+          
+          if (element.classList.contains('parallaxHigh')){
+            let movingSpeed = value*0.2
+            element.style.top = `${movingSpeed}px`
+          }
+          else if (element.classList.contains('parallaxMedium')){
+            let movingSpeed = value*0.1
+            element.style.top = `${movingSpeed}px`
+          }
+          else if (element.classList.contains('parallaxSlow')){
+            let movingSpeed = value*0.05
+            element.style.top = `${movingSpeed}px`
+          }
+        })
+
         i++
       });
     }
