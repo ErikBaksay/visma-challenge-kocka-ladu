ba9d7d0b9e4dc83ae3f57002187be9e3dcba5c11
Erik Baksay <baksayerik@gmail.com>
2022-04-23 17:46:06 +0200

    Bug Fix

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,34 +9,13 @@ import { images_data } from 'src/database';
 export class GalleryComponent implements OnInit {
 
   images_data = images_data
-<<<<<<< Updated upstream
-  parallax_types = ['parallaxSlow', 'parallaxMedium', 'parallaxHigh']
-=======
   parallax_types = ['parallaxSlow','parallaxMedium','parallaxHigh']
->>>>>>> Stashed changes
 
   constructor() { }
 
   ngOnInit(): void {
-<<<<<<< Updated upstream
-    function getRandomInt(max: number) {
-      return Math.floor(Math.random() * max);
-    }
-
-    console.log(getRandomInt(30) + 50);
-
-    const imageCards = document.getElementsByClassName("imageCard")
-
-    for (var i = 0; i < imageCards.length; i++) {
-      console.log(imageCards[i]);
-    }
   }
-}
-=======
-
-    }
     getRandomInt(max:number) {
       return Math.floor(Math.random() * max);
     }
 }
->>>>>>> Stashed changes
