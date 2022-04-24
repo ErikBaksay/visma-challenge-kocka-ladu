94852fb889b5f49405e4d688352d9ff51a931909
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2022-04-24 03:54:26 +0200

    Added default not found response to __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_mysqldb import MySQL
 from core import api as core_api
 import core.constants as constants
+import core.functions as functions
 
 app = Flask(__name__)
 
@@ -11,7 +12,13 @@ app.config['MYSQL_PASSWORD'] = constants.DB_PASSWORD
 app.config['MYSQL_DB'] = constants.DB_DATABASE
 app.config['MYSQL_PORT'] = constants.DB_PORT
 
+
+{"post_title", "description", "date", "images"}
+
 mysql = MySQL(app)
+
+not_found_response = functions.create_response(constants.RESPONSE_TYPES.ERROR, "not_found", "This resource was not found.", 404)
+not_found_response = functions.respond(not_found_response)
 
 
 @app.route("/")
@@ -19,11 +26,12 @@ def hello_world():
     return "AA"
 
 
-@app.route("/api/<user_request>", methods=["GET", "POST"])
-def api(user_request):
+@app.route('/api/<user_request>', defaults={'category': None}, methods=["GET", "POST"])
+@app.route("/api/<user_request>/<category>", methods=["GET", "POST"])
+def api(user_request, category):
     if user_request == "upload":
         return core_api.upload(request, mysql)
-    return user_request
+    return not_found_response
 
 
 if __name__ == "__main__":
